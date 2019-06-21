0bdb5c7686724309a3acf368f55f8d70db5e7005
ryanden2018 <denlinger.ryan@gmail.com>
2019-06-21 15:52:32 -0500

    done

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,24 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case "ADD_QUOTE":
+      return [...state.map( quote => {return {...quote}}),
+               {...action.quote}]
+    case "REMOVE_QUOTE":
+      var filteredState = state.filter( quote => quote.id !== action.quoteId );
+      return [...filteredState.map( quote => {return {...quote}})]
+    case "UPVOTE_QUOTE":
+      var filteredState = state.filter( quote => quote.id !== action.quoteId );
+      var origQuote = state.find( quote => quote.id === action.quoteId);
+      return [...filteredState.map( quote => {return {...quote}}),
+              {...origQuote, votes: origQuote.votes+1}]
+    case "DOWNVOTE_QUOTE":
+      var filteredState = state.filter( quote => quote.id !== action.quoteId );
+      var origQuote = state.find( quote => quote.id === action.quoteId);
+      return [...filteredState.map( quote => {return {...quote}}),
+              {...origQuote, votes: Math.max(origQuote.votes-1,0)}]
+    default:
+      return state;
+  }
 }
